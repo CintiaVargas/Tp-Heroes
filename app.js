2d62efc036da2a832c6c9cd801bb9df9a1056706
Cintia <cintiavargasmq@gmail.com>
2021-06-24 18:24:16 -0300

    Termino views

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+app.use(express.static('public/images'));
+
 app.listen(3030, ()=>console.log('Servidor corriendo'));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, './views/index.html')));
